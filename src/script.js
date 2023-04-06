6b134e252ccacd69e61d26183305f30a16a4d81d
Renata <110370964+An-Renata@users.noreply.github.com>
2023-04-06 10:27:42 +0300

    implementing about page

--- src/script.js
+++ src/script.js
@@ -2,7 +2,27 @@
 
 const links = document.querySelector(".nav-container");
 const navButton = document.querySelector(".hamburger-menu");
+const accordionHeaders = document.querySelectorAll(".accordion-heading");
 
 navButton.addEventListener("click", () => {
   links.classList.toggle("active");
+});
+
+accordionHeaders.forEach((accordionHeader) => {
+  accordionHeader.addEventListener("click", () => {
+    const currentlyActive = document.querySelector(".accordion-heading.active");
+    if (currentlyActive && currentlyActive !== accordionHeader) {
+      currentlyActive.classList.toggle("active");
+      currentlyActive.nextElementSibling.style.maxHeight = 0;
+    }
+
+    accordionHeader.classList.toggle("active");
+    const accordionBody = accordionHeader.nextElementSibling;
+
+    if (accordionHeader.classList.contains("active")) {
+      accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
+    } else {
+      accordionBody.style.maxHeight = 0;
+    }
+  });
 });
